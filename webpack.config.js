3cfdf753f944cc1c2cb558bd6c5087e4b97332c7
Stanley Makazhe <devt3000@gmail.com>
2018-07-10 21:08:55 +0200

    Code Splitting

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,12 @@ const port = process.env.PORT || 3000;
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        vendor: ['semantic-ui-react'],
+        app: './src/index.js'
+    },
     output: {
-        filename: 'bundle.[hash].js',
+        filename: '[name].[hash].js',
         publicPath: '/'
     },
     devtool: 'inline-source-map',
@@ -38,6 +41,18 @@ module.exports = {
                 ]
             }
         ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    chunks: 'initial',
+                    test: 'vendor',
+                    name: 'vendor',
+                    enforce: true
+                }
+            }
+        }
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
