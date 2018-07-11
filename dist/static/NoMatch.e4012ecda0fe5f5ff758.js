e9e2e7605724d3254b598edb2d00d7a7028839a3
Stanley Makazhe <devt3000@gmail.com>
2018-07-11 12:47:38 +0200

    Production Config

---
+++ dist/static/NoMatch.e4012ecda0fe5f5ff758.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{176:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u=o(t(0)),l=t(55),a=o(t(72));function o(e){return e&&e.__esModule?e:{default:e}}n.default=function(){return u.default.createElement(a.default,null,u.default.createElement(l.Icon,{name:"minus circle",size:"big"}),u.default.createElement("strong",null,"Page not found!"))}}}]);
+//# sourceMappingURL=NoMatch.e4012ecda0fe5f5ff758.js.map
